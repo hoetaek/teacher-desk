09c2ff458ff1872c50cabed88d152f2779390797
hoetaek <hoetaek@student.snue.ac.kr>
2022-05-14 09:50:54 +0900

    refactor: korean support with less duplicate code

--- wordsearch.py
+++ wordsearch.py
@@ -3,6 +3,7 @@ import json
 import os
 import random
 from statistics import mean
+from typing import List, Tuple
 
 import hgtk
 import requests
@@ -29,50 +30,97 @@ class Direction(Enum):
     BACKWARD = 3
 
 
+class Language(Enum):
+    KOREAN = 1
+    ENGLISH = 2
+
+
 class PuzzleData:
-    def __init__(self, width, height):
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        words,
+        lang: Language,
+        is_uppercase: bool = False,
+        is_scramble: bool = False,
+    ):
         self.width = width
         self.height = height
-        self.puzzle = [[0 for _ in range(width)] for _ in range(height)]
-        self.words = list()
 
-    def enter(self, words):
+        self.puzzle = [[0 for _ in range(width)] for _ in range(height)]
         self.words = words
-        for word in words:
-            word_enter_succeed = False
+
+        if lang == Language.KOREAN and is_uppercase == True:
+            raise ValueError("There is no uppercase in Korean")
+        self.lang = lang
+        self.is_uppercase = is_uppercase
+        self.is_scramble = is_scramble
+
+    def make(self):
+        self.__make_heading()
+        self.__make_puzzle()
+        self.__make_hint()
+
+    def __make_heading(self):
+        match self.lang:
+            case Language.KOREAN:
+                self.heading = "낱말 찾기"
+            case Language.ENGLISH:
+                self.heading = "Word Search"
+
+    def __make_hint(self):
+        self.hint = []
+        for word in self.words:
+            if self.is_uppercase:
+                word = word.upper()
+            if self.is_scramble:
+                match self.lang:
+                    case Language.ENGLISH:
+                        spelling = [i for i in word]
+                        shuffle(spelling)
+                        word = "".join(spelling)
+                    case Language.KOREAN:
+                        chosung_word = ""
+                        for chr in word:
+                            chosung = hgtk.letter.decompose(chr)[0]
+                            chosung_word += chosung
+                        word = chosung_word
+            self.hint.append(word)
+
+    def __make_puzzle(self):
+        for word in self.words:
+            entering_word_succeed = False
             print(word)
 
             try_num = 0
             max_try = 30
-            while not word_enter_succeed:
+            while not entering_word_succeed:
                 print(try_num)
-                puzzle_option = PuzzleOption()
+                puzzle_option = PuzzleDifficultyOption()
                 puzzle_option.get_option()
-                word_positions = WordPosition(word).get_word_positions(
-                    self.width,
-                    self.height,
-                    len(word),
+                word_positions = WordPosition(
+                    word, self.width, self.height
+                ).get_word_positions(
                     puzzle_option.x_direction,
                     puzzle_option.y_direction,
                 )
-                word_enter_succeed = self.fill_word(word, word_positions)
+                if self.word_position_exists(word, word_positions):
+                    self.fill_word(word, word_positions)
+                    entering_word_succeed = True
                 try_num += 1
                 if try_num > max_try:
                     self.__empty_puzzle()
-                    self.enter(words)
+                    self.make_puzzle(english_words)
 
         self.answer = copy.deepcopy(self.puzzle)
         self.fill_random_letters()
         [print(i) for i in self.puzzle]
 
-    def fill_word(self, word, word_positions):
-        if self.word_position_exists(word, word_positions):
-            x_y_seperated_positions = list(zip(*word_positions))
-            for letter, x, y in zip(word, *x_y_seperated_positions):
-                self.puzzle[y][x] = letter
-            return True
-        else:
-            return False
+    def fill_word(self, word: str, word_positions):
+        x_y_seperated_positions = list(zip(*word_positions))
+        for letter, x, y in zip(word, *x_y_seperated_positions):
+            self.puzzle[y][x] = letter.upper() if self.is_uppercase else letter
 
     def fill_random_letters(self):
         for i in range(self.height):
@@ -81,24 +129,26 @@ class PuzzleData:
                 if self.puzzle[i][j] == 0:
                     self.puzzle[i][j] = fill_alph
 
-    def get_random_letter(self, korean=False):
-        f = open("random_words.txt", "r")
-        data = f.read()
-        regex_f = r"[가-힣]+"
-        search_target_f = data
-        data = "".join(list(set(re.findall(regex_f, search_target_f))))
-        if korean:
-            fill_alph = random.choice(data)
-        else:
-            random.choice(string.ascii_lowercase)
+    def get_random_letter(self):
+        fill_alph: str
+        match self.lang:
+            case Language.KOREAN:
+                f = open("random_words.txt", "r")
+                data = f.read()
+                regex_f = r"[가-힣]+"
+                search_target_f = data
+                data = "".join(list(set(re.findall(regex_f, search_target_f))))
+                fill_alph = random.choice(data)
+            case Language.ENGLISH:
+                fill_alph = random.choice(string.ascii_lowercase)
         return fill_alph
 
     def word_position_exists(self, word, word_positions):
-        word_zero = "0" * len(word)
+        word_made_in_zero = "0" * len(word)
         word_in_puzzle = ""
         for x, y in word_positions:
             word_in_puzzle += str(self.puzzle[y][x])
-        if word_zero == word_in_puzzle:
+        if word_made_in_zero == word_in_puzzle:
             return True
         else:
             for i in range(len(word)):
@@ -111,8 +161,10 @@ class PuzzleData:
 
 
 class WordPosition:
-    def __init__(self, word):
-        self.word = word
+    def __init__(self, word, width, height):
+        self.word_length = len(word)
+        self.width = width
+        self.height = height
 
     def __get_positions(
         self, total_length: int, word_length: int, direction: Direction
@@ -131,22 +183,21 @@ class WordPosition:
                 positions = [
                     i for i in range(start_position, start_position - word_length, -1)
                 ]
-        print(positions)
         return positions
 
     def get_word_positions(
-        self, width, height, word_length, x_direction: Direction, y_direction: Direction
-    ):
+        self, x_direction: Direction, y_direction: Direction
+    ) -> List[Tuple[int, int]]:
         word_positions = list(
             zip(
-                self.__get_positions(width, word_length, x_direction),
-                self.__get_positions(height, word_length, y_direction),
+                self.__get_positions(self.width, self.word_length, x_direction),
+                self.__get_positions(self.height, self.word_length, y_direction),
             )
         )
         return word_positions
 
 
-class PuzzleOption:
+class PuzzleDifficultyOption:
     def __init__(self) -> None:
         pass
 
@@ -155,26 +206,16 @@ class PuzzleOption:
         self.y_direction = Direction.STEADY  # random.choice(list(Direction))
 
 
-class Language(Enum):
-    KOREAN = 1
-    ENGLISH = 2
-
-
 class Worksheet:
-    def __init__(self, lang: Language = Language.ENGLISH):
-        self.lang = lang
-        self.heading = "Word Puzzle"
-        if lang == Language.KOREAN:
-            self.heading = "낱말 찾기"
-        self.uppercase = True
-        self.chosung_scramable = True
-
-    def configure_settings(self, puzzle_data: PuzzleData):
+    def __init__(self, puzzle_data: PuzzleData):
         self.width = puzzle_data.width
         self.height = puzzle_data.height
+        self.heading = puzzle_data.heading
         self.puzzle = puzzle_data.puzzle
+        self.hint: List[str] = puzzle_data.hint
         self.answer = puzzle_data.answer
 
+    def configure_settings(self):
         # write to docx file
         # Write to docx to puzzle.docx
         self.document = Document()
@@ -186,15 +227,11 @@ class Worksheet:
             section.left_margin = Cm(2.3)
             section.right_margin = Cm(2.3)
 
-        self.grade: int = "__"
-        self.class_num: int = "__"
-
-    def add_heading(self):
+    def add_heading(self, grade, class_num):
         head = self.document.add_heading(self.heading, 0)
         head.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        para_belong = self.document.add_paragraph(
-            f"{self.grade}학년 {self.class_num}반 이름: _______"
-        )
+        belong = f"{grade}학년 {class_num}반 이름: _______"
+        para_belong = self.document.add_paragraph(belong)
         para_belong.alignment = WD_ALIGN_PARAGRAPH.RIGHT
 
     def add_puzzle(self):
@@ -216,7 +253,7 @@ class Worksheet:
             for j, cell in enumerate(row.cells):
                 #####가로 길이 정하기!
                 cell.width = Inches(5)
-                cell.text = self.get_text_from_puzzle(self.puzzle, i, j)
+                cell.text = self.puzzle[i][j]
                 for paragraph in cell.paragraphs:
                     #####가운데 정렬!!
                     paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
@@ -228,14 +265,8 @@ class Worksheet:
                 tcVAlign.set(qn("w:val"), "center")
                 tcPr.append(tcVAlign)
 
-    def get_text_from_puzzle(self, puzzle, i, j):
-        if self.uppercase and self.lang == Language.ENGLISH:
-            return str(puzzle[i][j]).upper()
-        else:
-            return str(puzzle[i][j])
-
     def add_hint_in_table(self):
-        word_num = len(words)
+        word_num = len(self.hint)
         if word_num <= 15:
             size = 5
         elif word_num <= 21:
@@ -243,7 +274,7 @@ class Worksheet:
         else:
             size = 6
         hint_table = self.document.add_table(
-            rows=(len(words) + size - 1) // size, cols=size, style="Table Grid"
+            rows=(word_num + size - 1) // size, cols=size, style="Table Grid"
         )
         hint_table.alignment = WD_TABLE_ALIGNMENT.CENTER
 
@@ -260,31 +291,9 @@ class Worksheet:
                 index = i * size + j
 
                 # 단어 수 만큼 반복하기
-                if index < len(words):
+                if index < word_num:
                     for paragraph in cell.paragraphs:
-                        # 초성 또는 scramble이 켜져 있는 경우
-                        if self.chosung_scramable:
-                            word = words[index]
-                            if self.lang == Language.KOREAN:
-                                cho_word = ""
-                                for chr in word:
-                                    chosung_scramable = hgtk.letter.decompose(chr)[0]
-                                    cho_word += chosung_scramable
-                                run = paragraph.add_run(cho_word)
-                            else:
-                                # 사진 있고 영어고 scramble인 경우
-                                spelling = [i for i in word]
-                                shuffle(spelling)
-                                scrambled_word = "".join(spelling)
-                                if self.uppercase:
-                                    run = paragraph.add_run(scrambled_word.upper())
-                                else:
-                                    run = paragraph.add_run(scrambled_word)
-                        else:
-                            if self.uppercase and not self.korean:
-                                run = paragraph.add_run(words[index].upper())
-                            else:
-                                run = paragraph.add_run(words[index])
+                        run = paragraph.add_run(self.hint[index])
                         font = run.font
                         font.name = "Arial"
                         font.size = Pt(13)
@@ -301,7 +310,6 @@ class Worksheet:
 
     def add_hint(self):
         # 힌트 테이블 만들기
-        # 사진이 안 들어가고 영어인 경우
         if self.lang == Language.ENGLISH:
             hint_table = self.document.add_table(rows=1, cols=1, style="Table Grid")
             hint_table.alignment = WD_TABLE_ALIGNMENT.CENTER
@@ -313,16 +321,7 @@ class Worksheet:
             hint_trHeight.set(qn("w:hRule"), "atLeast")
             hint_trPr.append(hint_trHeight)
             hint_table_cell = hint_table_row.cells[0]
-            hint = ""
-
-            for word in words:
-                if self.uppercase:
-                    word = word.upper()
-                if self.chosung_scramable:
-                    spelling = [i for i in word]
-                    shuffle(spelling)
-                    word = "".join(spelling)
-                hint += word + ", "
+            hint = ", ".join(self.hint)
             hint_table_cell.width = Inches(100)
             for paragraph in hint_table_cell.paragraphs:
                 paragraph.add_run(hint.strip(", "))
@@ -332,59 +331,6 @@ class Worksheet:
             tcVAlign = OxmlElement("w:vAlign")
             tcVAlign.set(qn("w:val"), "center")
             tcPr.append(tcVAlign)
-
-        else:
-            # 사진이 안 들어가고 한글인 경우
-            if self.chosung_scramable:
-                hint_table = self.document.add_table(rows=1, cols=1, style="Table Grid")
-                hint_table.alignment = WD_TABLE_ALIGNMENT.CENTER
-                hint_table_row = hint_table.rows[0]
-                hint_tr = hint_table_row._tr
-                hint_trPr = hint_tr.get_or_add_trPr()
-                hint_trHeight = OxmlElement("w:trHeight")
-                hint_trHeight.set(qn("w:val"), "1000")
-                hint_trHeight.set(qn("w:hRule"), "atLeast")
-                hint_trPr.append(hint_trHeight)
-                hint_table_cell = hint_table_row.cells[0]
-                hint = ""
-                for word in words:
-                    cho_word = ""
-                    for chr in word:
-                        chosung_scramable = hgtk.letter.decompose(chr)[0]
-                        cho_word += chosung_scramable
-                    hint += cho_word + ", "
-                hint_table_cell.width = Inches(100)
-                for paragraph in hint_table_cell.paragraphs:
-                    paragraph.add_run(hint.strip(", "))
-                    paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-                tc = hint_table_cell._tc
-                tcPr = tc.get_or_add_tcPr()
-                tcVAlign = OxmlElement("w:vAlign")
-                tcVAlign.set(qn("w:val"), "center")
-                tcPr.append(tcVAlign)
-            else:
-                hint_table = self.document.add_table(rows=1, cols=1, style="Table Grid")
-                hint_table.alignment = WD_TABLE_ALIGNMENT.CENTER
-                hint_table_row = hint_table.rows[0]
-                hint_tr = hint_table_row._tr
-                hint_trPr = hint_tr.get_or_add_trPr()
-                hint_trHeight = OxmlElement("w:trHeight")
-                hint_trHeight.set(qn("w:val"), "1000")
-                hint_trHeight.set(qn("w:hRule"), "atLeast")
-                hint_trPr.append(hint_trHeight)
-                hint_table_cell = hint_table_row.cells[0]
-                hint = ""
-                for word in words:
-                    hint += word + ", "
-                hint_table_cell.width = Inches(100)
-                for paragraph in hint_table_cell.paragraphs:
-                    paragraph.add_run(hint.strip(", "))
-                    paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-                tc = hint_table_cell._tc
-                tcPr = tc.get_or_add_tcPr()
-                tcVAlign = OxmlElement("w:vAlign")
-                tcVAlign.set(qn("w:val"), "center")
-                tcPr.append(tcVAlign)
 
     def write(self):
         self.add_heading()
@@ -417,7 +363,7 @@ class Worksheet:
             for j, cell in enumerate(row.cells):
                 #####가로 길이 정하기!
                 cell.width = Inches(8)
-                cell.text = self.get_text_from_puzzle(self.answer, i, j)
+                cell.text = str(self.answer[i][j])
                 for paragraph in cell.paragraphs:
                     #####가운데 정렬!!
                     paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
@@ -441,8 +387,7 @@ class Worksheet:
 
 
 if __name__ == "__main__":
-    puzzle = PuzzleData(20, 20)
-    words = [
+    english_words = [
         "hello",
         "python",
         "anything",
@@ -469,12 +414,13 @@ if __name__ == "__main__":
         "gravity",
         "feelings",
     ]
-
-    puzzle.enter(words)
-    worksheet = Worksheet()
-    worksheet.configure_settings(puzzle)
+    korean_words = ["안녕", "홓호", "정대준", "민지홍", "나성훈"]
+    puzzle_data = PuzzleData(20, 20, korean_words, Language.KOREAN, False, True)
+    puzzle_data.make()
+    worksheet = Worksheet(puzzle_data)
+    worksheet.configure_settings()
     worksheet.write()
-    worksheet.write_answer(puzzle)
+    worksheet.write_answer(puzzle_data)
     worksheet.save("filename")
 
     # document = Document()
