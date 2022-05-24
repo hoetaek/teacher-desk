415fa04087ec2c71a75a2a3a232ce96638d49c5b
hoetaekpro <hoetaek@student.snue.ac.kr>
2022-05-24 20:53:26 +0900

    feat: basic navbar

--- teacher_desk_vue/src/App.vue
+++ teacher_desk_vue/src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
+    <nav class="navbar is-white">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>교사의 책상</strong></router-link>
+        <router-link to="/" class="navbar-item">
+          <img src="./assets/교사의책상.png" >
+        </router-link>
 
         <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
-          </a>
+          
+        </a>
       </div>
+      
+    </nav>
+    <nav class="navbar is-white">
+      
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
-        <div class="navbar-end">
-          <router-link to="/wordsearch" class="navbar-item">낱말 찾기</router-link>
+        <div class="navbar-start">
+          <router-link to="/" class="navbar-item">🏠홈</router-link>
+          <router-link to="/wordsearch" class="navbar-item">📕낱말 찾기</router-link>
+          <button v-on:click="isEnHidden = !isEnHidden">Hide the text below</button>
+          <div class="is-align-items-center is-flex">
+              <router-link to="/wordsearch/kr" class="navbar-item button is-small is-rounded is-primary"  v-if="!isKrHidden">한국어</router-link>
+          </div>
+          <div class="is-align-items-center is-flex">
+            <router-link to="/wordsearch/en" class="navbar-item button is-small is-rounded is-primary"   v-if="!isEnHidden">Eng</router-link>
+          </div>
           <router-link to="/otherpuzzle" class="navbar-item">다른 퍼즐</router-link>
+
+         
+        </div>
+        <div class="navbar-end">
+    
 
           <div class="navbar-item">
             <div class="buttons">
@@ -42,6 +62,8 @@ export default{
   data(){
     return {
       showMobileMenu:false, 
+      isKrHidden: false,
+      isEnHidden: true,
     }
   }
   
