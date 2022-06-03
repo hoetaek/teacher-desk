63ee50ae5c00d76ddeb90204d0ab3d094b021fe0
hoetaek <hoetaek@student.snue.ac.kr>
2022-06-03 12:14:55 +0900

    feat: worksheet word input

--- teacher_desk_vue/src/App.vue
+++ teacher_desk_vue/src/App.vue
@@ -1,33 +1,31 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-white mt-5 navbar-center is-spaced">
-      <div class="navbar-brand">
-        <router-link to="/" class="navbar-item">
-          <img src="./assets/brand-cropped.svg" width="180" />
-        </router-link>
+    <div class="margin-center">
+      <nav class="navbar is-white mt-5 is-spaced">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item">
+            <img src="./assets/brand-cropped.svg" width="180" />
+          </router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
-          @click="showMobileMenu = !showMobileMenu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
-        <div class="navbar-end">
-          <router-link to="/" class="navbar-item heading is-4 mr-4">소개</router-link>
-          <router-link to="/wordsearch" class="navbar-item heading is-4 mr-4">활동지 만들기</router-link>
-          <router-link to="/otherpuzzle" class="navbar-item heading is-4 mr-4">블로그</router-link>
-          <router-link to="/ask" class="navbar-item heading is-4 mr-4">건의사항</router-link>
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+            @click="showMobileMenu = !showMobileMenu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
+        <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
+          <div class="navbar-end">
+            <router-link to="/about" class="navbar-item heading is-4 mr-4">소개</router-link>
+            <router-link to="/worksheet" class="navbar-item heading is-4 mr-4">활동지 만들기</router-link>
+            <router-link to="/blog" class="navbar-item heading is-4 mr-4">블로그</router-link>
+            <router-link to="/suggestions" class="navbar-item heading is-4 mr-4">건의사항</router-link>
+          </div>
 
-      </div>
-    </nav>
-
-    <section class="section is-medium">
+        </div>
+      </nav>
       <router-view />
-    </section>
-
+    </div>
     <!-- <footer class="footer">
       <p class="has-text-centered">Copyright (c) 2022</p>
     </footer> -->
---
+++ teacher_desk_vue/src/views/WorksheetView.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="worksheet">
+
+        <section class="section">
+            <div class="mb-6">
+                <div class="title is-4 has-text-grey-light">아래 내용을 입력해주세요.</div>
+            </div>
+            <div class="title is-3">01 아이들이 찾게 될 단어</div>
+            <div class="mx-6 mb-6">
+                <form class="has-text-centered" method="get">
+                    <div class="tile is-ancestor">
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                    </div>
+                    <div class="tile is-ancestor">
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                        <div class="tile is-parent">
+                            <article class="tile is-child notification has-background-grey-lighter is-rounded">
+                                <input class="input is-static center-block" type="text" placeholder="단어">
+                            </article>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="title is-3">02 문제의 난이도</div>
+        </section>
+
+    </div>
+</template>
