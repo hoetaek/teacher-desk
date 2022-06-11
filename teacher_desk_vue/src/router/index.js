c183e06eb87a3257219f3eae9799d47bc556ba3b
hoetaek <hoetaek@student.snue.ac.kr>
2022-06-11 09:13:52 +0900

    feat: configure router for each pages

--- teacher_desk_vue/src/router/index.js
+++ teacher_desk_vue/src/router/index.js
@@ -10,32 +10,32 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
     path: '/wordsearch',
     name: 'wordsearch',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/WorksheetView.vue')
   }, {
     path: '/wordsearch/en',
     name: 'english_wordsearch',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/WordsearchEnglishView.vue')
   }, {
     path: '/wordsearch/kr',
     name: 'korean_wordsearch',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/WordsearchKoreanView.vue')
+  }, {
+    path: '/puzzle',
+    name: 'puzzle',
+    component: () => import(/* webpackChunkName: "about" */ '../views/PuzzleView.vue')
+  }, {
+    path: '/blog',
+    name: 'blog',
+    component: () => import(/* webpackChunkName: "about" */ '../views/BlogView.vue')
+  }, {
+    path: '/suggestions',
+    name: 'suggestions',
+    component: () => import(/* webpackChunkName: "about" */ '../views/SuggestionsView.vue')
   }
 ]
 
---
+++ teacher_desk_vue/src/views/PuzzleView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="puzzle">
+    <h1>This is an puzzle page</h1>
+  </div>
+</template>
