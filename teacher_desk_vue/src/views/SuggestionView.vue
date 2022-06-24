8dbb4e977f225329bc951a9957d51386a6b53cf2
hoetaek <hoetaek@student.snue.ac.kr>
2022-06-24 12:01:38 +0900

    feat: suggestion page basic format

--- teacher_desk_vue/src/views/SuggestionView.vue
+++ teacher_desk_vue/src/views/SuggestionView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="suggestions">
-    <h1>This is an Board page</h1>
+    <section class="section has-text-centered">
+      <div class="title is-2">반복적으로 만들고 있는 <br>
+        활동지나 학습자료가 있나요?</div>
+      <p class="title is-2 has-text-info">저희가 대신 만들어드릴게요!</p>
+
+      <div
+        class="px-5"
+        style="width:50%; border: solid 1px; border-radius: 25px; margin: auto;"
+      >
+        <textarea
+          class="textarea is-medium"
+          placeholder="이런 걸 만들어주면 좋겠어요."
+        ></textarea>
+        <div class="box has-text-right">
+          <button
+            class="button is-info"
+            @click="$router.push('/worksheet')"
+          > <span class="icon">
+              <i class="fas fa-plus"></i>
+            </span><span>남기기</span>
+          </button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+<style>
+.textarea {
+  border: none;
+}
+
+.box {
+  box-shadow: none;
+}
+</style>
