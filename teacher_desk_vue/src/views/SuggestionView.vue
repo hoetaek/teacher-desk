fda75c931fb9577ae5781c9b57a62dfe1d09326a
hoetaek <hoetaek@student.snue.ac.kr>
2022-06-26 12:31:45 +0900

    feat: change textarea size placeholder size

--- teacher_desk_vue/src/views/SuggestionView.vue
+++ teacher_desk_vue/src/views/SuggestionView.vue
@@ -3,14 +3,12 @@
     <section class="section has-text-centered">
       <div class="title is-2">반복적으로 만들고 있는 <br>
         활동지나 학습자료가 있나요?</div>
-      <p class="title is-2 has-text-info">저희가 대신 만들어드릴게요!</p>
+      <p class="title is-2 has-text-info my-6">저희가 대신 만들어드릴게요!</p>
 
-      <div
-        class="px-5"
-        style="width:50%; border: solid 1px; border-radius: 25px; margin: auto;"
-      >
+      <div class="container px-5">
         <textarea
-          class="textarea is-medium"
+          class="textarea"
+          rows="2"
           placeholder="이런 걸 만들어주면 좋겠어요."
         ></textarea>
         <div class="box has-text-right">
@@ -23,6 +21,7 @@
           </button>
         </div>
       </div>
+
     </section>
   </div>
 </template>
@@ -38,6 +37,18 @@
   box-shadow: none;
 
   resize: none;
+
+  font-size: 25px;
+}
+
+textarea::placeholder {
+  font-size: 25px;
+}
+
+.container {
+  width: 40%;
+  border: solid 2px;
+  border-radius: 25px;
 }
 
 .box {
