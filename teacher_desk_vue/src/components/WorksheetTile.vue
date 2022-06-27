d30534c69954efad02cf823f6a3809e91068bdea
hoetaek <hoetaek@student.snue.ac.kr>
2022-06-27 10:42:36 +0900

    feat: wordsearch tile

--- teacher_desk_vue/src/components/WorksheetTile.vue
+++ teacher_desk_vue/src/components/WorksheetTile.vue
@@ -1,29 +1,43 @@
 <template >
-  <div style="width: 246.32px; height: 635.56px;">
-    <div class="
-    tile
-    is-ancestor">
-      <div
-        class="tile is-parent"
-        style="width: 246.32px; height: 381.23px;"
-      >
-        <article class="tile is-child notification has-background-grey-lighter is-rounded width:246">
-          image
-        </article>
+  <div>
+    <div class="card">
+      <div class="container p-4">
+        <div class="card-image">
+          <figure class="image is-square">
+            <img
+              src="../assets/wordsearch_image.png"
+              alt="Image"
+            >
+          </figure>
+        </div>
+      </div>
+      <div class="card-content">
+
+        <div class="content">
+          <h1
+            class="title has-text-left"
+            @click="$router.push('/wordsearch')"
+            @mouseover="isHover=true"
+            @mouseleave="isHover=false"
+            style="cursor: pointer"
+            :class="classObject"
+          >낱말 찾기</h1>
+          <span class="subtitle is-4">주어진 낱말에서 학습할 낱말 찾기</span>
+          <br>
+          <div class="level">
+            <div class="level-item level-right">
+              <div class="icon is-medium">
+                <i class="fas fa-heart"></i>
+              </div>
+              <span>10</span>
+
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
 
-    <div
-      @click="$router.push('/wordsearch')"
-      @mouseover="isHover=true"
-      @mouseleave="isHover=false"
-      style="cursor: pointer"
-      :class="classObject"
-    >낱말 찾기</div>
-    <div class="has-text-centered">주어진 낱말을
-      여러 낱말들 속에서
-      찾으며 주요 낱말을
-      학습할 수 있는 활동지입니다. </div>
   </div>
 </template>
 <script>
@@ -44,3 +58,17 @@ export default {
   },
 };
 </script>
+
+<style>
+.card {
+  box-shadow: none;
+  border: solid 3px;
+  border-radius: 20px;
+}
+
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- teacher_desk_vue/src/views/WorksheetView.vue
+++ teacher_desk_vue/src/views/WorksheetView.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="section level">
-    <worksheet-tile></worksheet-tile>
-    <worksheet-tile></worksheet-tile>
-    <worksheet-tile></worksheet-tile>
+  <div class="section">
+    <div class="columns is-variable is-8">
+      <div class="column">
+        <worksheet-tile></worksheet-tile>
+      </div>
+      <div class="column">
+        <worksheet-tile></worksheet-tile>
+      </div>
+      <div class="column">
+        <worksheet-tile></worksheet-tile>
+      </div>
+
+    </div>
+
   </div>
 </template>
 <script>
