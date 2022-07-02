52752c013103dad203b2915ff9256e35e8f4c769
hoetaek <hoetaek@student.snue.ac.kr>
2022-07-02 21:13:22 +0900

    refactor: worksheet tile from map

--- teacher_desk_vue/src/views/WorksheetView.vue
+++ teacher_desk_vue/src/views/WorksheetView.vue
@@ -1,56 +1,27 @@
 <template>
   <div class="section">
     <div class="columns is-variable is-8">
-      <div class="column">
+      <div
+        class="column"
+        v-for="(worksheet, index) in worksheetData"
+        :key="index"
+      >
         <worksheet-tile
-          :filename="wordsearchPath"
-          :liked="true"
+          :filename="worksheet.filename"
+          :liked="worksheet.liked"
+          :isNotReady="worksheet.isNotReady"
         >
           <template #title>
-            낱말 찾기
+            {{ worksheet.title }}
           </template>
           <template #description>
-            주어진 낱말에서 학습할 낱말 찾기
+            {{ worksheet.description }}
           </template>
           <template #heartNum>
-            10
+            {{ worksheet.heartNum }}
           </template>
         </worksheet-tile>
       </div>
-      <div class="column">
-        <worksheet-tile
-          :filename="wordsearchPath"
-          :liked="false"
-          :isNotReady="true"
-        ><template #title>
-
-            말판 놀이
-          </template>
-          <template #description>
-            주사위를 굴러 먼저 도착하기
-          </template>
-          <template #heartNum>
-            7
-          </template>
-        </worksheet-tile>
-      </div>
-      <div class="column">
-        <worksheet-tile
-          :filename="wordsearchPath"
-          :liked="false"
-          :isNotReady="true"
-        ><template #title>
-            도블
-          </template>
-          <template #description>
-            같은 것 먼저 외치기
-          </template>
-          <template #heartNum>
-            20
-          </template>
-        </worksheet-tile>
-      </div>
-
     </div>
 
   </div>
@@ -60,6 +31,32 @@ import WorksheetTile from "@/components/WorksheetTile.vue";
 export default {
   data() {
     return {
+      worksheetData: [
+        {
+          filename: "wordsearch_image.png",
+          liked: true,
+          isNotReady: false,
+          title: "낱말 찾기",
+          description: "주어진 낱말에서 학습할 낱말 찾기",
+          heartNum: 10,
+        },
+        {
+          filename: "wordsearch_image.png",
+          liked: true,
+          isNotReady: true,
+          title: "말판 놀이",
+          description: "주사위를 굴러 먼저 도착하기",
+          heartNum: 7,
+        },
+        {
+          filename: "wordsearch_image.png",
+          liked: true,
+          isNotReady: true,
+          title: "도블",
+          description: "같은 것 먼저 외치기",
+          heartNum: 20,
+        },
+      ],
       wordsearchPath: "wordsearch_image.png",
     };
   },
