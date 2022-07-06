dafcd3e759eec12df2893870e41b09e267408cd2
hoetaek <hoetaek@student.snue.ac.kr>
2022-07-06 23:57:08 +0900

    feat: make suggestion tile component

---
+++ teacher_desk_vue/src/components/SuggestionTile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div
+      class="tile is-child notification px-5"
+      :style="{'background-color': color}"
+    >
+      <p class="is-size-4 has-text-weight-bold">{{idx}}번째 건의사항</p>
+      {{ text }}
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    idx: Number,
+    text: String,
+    color: String,
+  },
+};
+</script>
